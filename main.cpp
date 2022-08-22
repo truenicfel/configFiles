2360c6ab43ae3f83a7096c2b516492c708c5655d
Nico Daßler <nico.dassler@fau.de>
2022-08-22 17:36:59 +0200

    Some tests with toml

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <toml.hpp>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    auto data = toml::parse("filename.toml");
+
+    toml::value tableTop = toml::get<toml::table>(data);
+    toml::value& table = toml::find(data, "mytable");
+
+    toml::value v{};    // table
+    v["super"] = "super";
+    tableTop.at("test") = v;
+    std::cout << tableTop << std::endl;
+//    std:: cout << data << std::endl;
+//
+//    // Create and open a text file
+//    std::ofstream MyFile("filename.toml");
+//
+//    // Write to the file
+//    MyFile << toml::format(data, 120, 17);
+//
+//    // Close the file
+//    MyFile.close();
+
     return 0;
 }
